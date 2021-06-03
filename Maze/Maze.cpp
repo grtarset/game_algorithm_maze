afcbb39c19b02d95e3d96362713a8bc4ec0e9aca
grtarset <84351671+grtarset@users.noreply.github.com>
2021-06-03 14:00:15 +0300

    updateUpdate Maze.cpp

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -2,110 +2,196 @@
 #include <list>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
 
 using namespace std;
 
+
 struct Target {
-	int h = 0;
-	int w = 0;
+	Target(int h = 0, int w = 0) : h(h), w(w) {}
+
+	int h;
+	int w;
 };
 
-int main()
+char space[40][40], space_fog[40][40];
+const char wall_ch = '#', way_ch = ' ', fog_ch = '+', player_ch = '*';
+const int size_h = sizeof(space) / sizeof(space[0]) - 1,
+size_w = sizeof(space[0]) / sizeof(space[0][0]) - 1;
+int key = 0, player_x = 1, player_y = 0, entrance_x, entrance_y = 0, exit_x, exit_y = 0;
+bool game = true;
+
+enum Control { STOP = 0, UP = 72, DOWN = 80, LEFT = 75, RIGHT = 77, ESC = 27 };
+Control ctr = STOP;
+
+
+void generate_maze()
 {
 	srand(time(NULL));
-	
+	int target_h = 1, target_w = 1, rand_way = 10,
+		up = 1, left = 2, down = 3, right = 4;
 	list<Target> target;
-	Target target_struct;	
+	target.push_back(Target(target_h, target_w));
 
-	char space[40][40], wall_ch = '#', way_ch = ' ';
-    	int size_h = sizeof(space) / sizeof(space[0]) - 1,
-    	size_w = sizeof(space[0]) / sizeof(space[0][0]) - 1,
-	target_h = 1, target_w = 1, rand_way = 10,
-	up = 1, left = 2, down = 3, right = 4;
-
-	target_struct.h = target_h;
-	target_struct.w = target_w;
-	target.push_back(target_struct);
-
-	for(int i = 0; i < size_h; i++)
-		for(int j = 0; j < size_w; j++)
+	for (int i = 0; i < size_h; i++)
+		for (int j = 0; j < size_w; j++)
 			space[i][j] = wall_ch;
 
-	space[1][0] = way_ch;
-	space[size_h - 2][size_w - 1] = way_ch;
-	
-	while(1)
+	while (1)
 	{
-		if(space[target_h - 2][target_w] == way_ch || target_h - 2 < 1)
-			if(space[target_h + 2][target_w] == way_ch || target_h + 2 > size_h - 1)
-				if(space[target_h][target_w - 2] == way_ch || target_w - 2 < 1) 
-					if(space[target_h][target_w + 2] == way_ch || target_w + 2 > size_w - 1)
+		if (space[target_h - 2][target_w] == way_ch || target_h - 2 < 1)
+			if (space[target_h + 2][target_w] == way_ch || target_h + 2 > size_h - 1)
+				if (space[target_h][target_w - 2] == way_ch || target_w - 2 < 1)
+					if (space[target_h][target_w + 2] == way_ch || target_w + 2 > size_w - 1)
 					{
 						target.pop_back();
-						if(target.empty()) break;
+						if (target.empty()) break;
 						target_h = target.back().h;
 						target_w = target.back().w;
 					}
-	
+
 		rand_way = rand() % 4 + 1;
-		if(rand_way == up)
-			if(target_h - 2 > 0)
-				if(space[target_h - 2][target_w] != way_ch)
-				{
-					space[target_h][target_w] = way_ch;
-					space[target_h - 1][target_w] = way_ch;
-					space[target_h - 2][target_w] = way_ch;
-					target_h -= 2;
-					target_struct.h = target_h;
-					target_struct.w = target_w;
-					target.push_back(target_struct);
-				}
+		if (rand_way == up && target_h - 2 > 0 && space[target_h - 2][target_w] != way_ch)
+		{
+			space[target_h][target_w] = way_ch;
+			space[target_h - 1][target_w] = way_ch;
+			space[target_h - 2][target_w] = way_ch;
+			target_h -= 2;
+			target.push_back(Target(target_h, target_w));
+		}
 
-		if(rand_way == left)
-			if(target_w - 2 > 0)
-				if(space[target_h][target_w - 2] != way_ch)
-				{
-					space[target_h][target_w] = way_ch;
-					space[target_h][target_w - 1] = way_ch;
-					space[target_h][target_w - 2] = way_ch;
-					target_w -= 2;
-					target_struct.h = target_h;
-					target_struct.w = target_w;
-					target.push_back(target_struct);
-				}
+		if (rand_way == left && target_w - 2 > 0 && space[target_h][target_w - 2] != way_ch)
+		{
+			space[target_h][target_w] = way_ch;
+			space[target_h][target_w - 1] = way_ch;
+			space[target_h][target_w - 2] = way_ch;
+			target_w -= 2;
+			target.push_back(Target(target_h, target_w));
+		}
 
-		if(rand_way == down)
-			if(target_h + 2 < size_h - 1)
-				if(space[target_h + 2][target_w] != way_ch)
-				{
-					space[target_h][target_w] = way_ch;
-					space[target_h + 1][target_w] = way_ch;
-					space[target_h + 2][target_w] = way_ch;
-					target_h += 2;
-					target_struct.h = target_h;
-					target_struct.w = target_w;
-					target.push_back(target_struct);
-				}
+		if (rand_way == down && target_h + 2 < size_h - 1 && space[target_h + 2][target_w] != way_ch)
+		{
+			space[target_h][target_w] = way_ch;
+			space[target_h + 1][target_w] = way_ch;
+			space[target_h + 2][target_w] = way_ch;
+			target_h += 2;
+			target.push_back(Target(target_h, target_w));
+		}
 
-		if(rand_way == right)
-			if(target_w + 2 < size_w - 1)
-				if(space[target_h][target_w + 2] != way_ch)
-				{
-					space[target_h][target_w] = way_ch;
-					space[target_h][target_w + 1] = way_ch;
-					space[target_h][target_w + 2] = way_ch;
-					target_w += 2;
-					target_struct.h = target_h;
-					target_struct.w = target_w;
-					target.push_back(target_struct);
-				}
+		if (rand_way == right && target_w + 2 < size_w - 1 && space[target_h][target_w + 2] != way_ch)
+		{
+			space[target_h][target_w] = way_ch;
+			space[target_h][target_w + 1] = way_ch;
+			space[target_h][target_w + 2] = way_ch;
+			target_w += 2;
+			target.push_back(Target(target_h, target_w));
+		}
 	}
 
-	for(int i = 0; i < size_h; i++)
+	int tmp;
+	while (true) {
+		tmp = rand() % (size_h - 2) + 1;
+		if (space[tmp][1] == way_ch) {
+			entrance_x = tmp;
+			player_x = entrance_x;
+			space[entrance_x][entrance_y] = way_ch;
+			break;
+		}
+	}
+	while (true) {
+		tmp = rand() % (size_h - 2) + 1;
+		if (space[size_h - 2][tmp] == way_ch) {
+			exit_x = tmp;
+			space[exit_x][exit_y] = way_ch;
+			break;
+		}
+	}
+	space[size_h - 2][size_w - 1] = way_ch;
+
+}
+
+void draw()
+{
+	system("cls");
+	for (int i = 0; i < size_h; i++)
 	{
-		for(int j = 0; j < size_w; j++)
-			cout << space[i][j];
+		for (int j = 0; j < size_w; j++)
+			cout << space_fog[i][j];
 		cout << endl;
+	}
+
+}
+
+void input()
+{
+	if (_kbhit)
+	{
+		key = _getch();
+		switch (key)
+		{
+		case UP:
+			ctr = UP;
+			break;
+		case DOWN:
+			ctr = DOWN;
+			break;
+		case LEFT:
+			ctr = LEFT;
+			break;
+		case RIGHT:
+			ctr = RIGHT;
+			break;
+		case ESC:
+			game = false;
+
+		}
+	}
+}
+
+void logic()
+{
+	if (ctr == UP && space[player_x - 1][player_y] != wall_ch && player_x > 1) { player_x--; ctr = STOP; }
+	if (ctr == DOWN && space[player_x + 1][player_y] != wall_ch && player_x < size_h) { player_x++; ctr = STOP; }
+	if (ctr == LEFT && space[player_x][player_y - 1] != wall_ch && player_y > 1) { player_y--; ctr = STOP; }
+	if (ctr == RIGHT && space[player_x][player_y + 1] != wall_ch && player_y < size_w) { player_y++; ctr = STOP; }
+
+	if (player_x - 1 > 0 && player_y > 0)
+		space_fog[player_x - 1][player_y - 1] = space[player_x - 1][player_y - 1];
+	if (player_x - 1 > 0)
+		space_fog[player_x - 1][player_y] = space[player_x - 1][player_y];
+	if (player_x - 1 > 0 && player_y < size_w - 1)
+		space_fog[player_x - 1][player_y + 1] = space[player_x - 1][player_y + 1];
+	if (player_y > 0)
+		space_fog[player_x][player_y - 1] = space[player_x][player_y - 1];
+	if (player_y < size_w - 1)
+		space_fog[player_x][player_y + 1] = space[player_x][player_y + 1];
+	if (player_x - 1 < size_h - 1 && player_y > 0)
+		space_fog[player_x + 1][player_y - 1] = space[player_x + 1][player_y - 1];
+	if (player_x - 1 < size_h - 1)
+		space_fog[player_x + 1][player_y] = space[player_x + 1][player_y];
+	if (player_x - 1 < size_h - 1 && player_y < size_w - 1)
+		space_fog[player_x + 1][player_y + 1] = space[player_x + 1][player_y + 1];
+
+	space_fog[player_x][player_y] = player_ch;
+}
+
+int main()
+{
+
+	generate_maze();
+
+	for (int i = 0; i < size_h; i++)
+		for (int j = 0; j < size_w; j++)
+		{
+			if (i == 0 || j == 0 || i == size_h - 1 || j == size_w - 1) space_fog[i][j] = wall_ch;
+			else space_fog[i][j] = fog_ch;
+		}
+
+	while (game)
+	{
+		logic();
+		draw();
+		input();
 	}
 
 	return 0;
